dd0ba0a9c8ffe970440e94ecedbb7780a6df81c6
Sajjad <sajad.abedi@veriff.net>
2022-09-28 16:05:34 +0300

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Sajad Abedi on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
